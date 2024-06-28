62f3f88010dc55745887cd86017840a7b2726feb
BlaiseAdrian <112819503+BlaiseAdrian@users.noreply.github.com>
2024-06-28 18:00:27 +0300

    Update constants.js

--- Models/constants.js
+++ Models/constants.js
@@ -2,14 +2,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const constantsSchema = new Schema({
-monthly_credit_rate: Number,
+one_point_value: Number,
 max_lending_rate: Number,
 min_lending_rate: Number,
 annual_tax_rate: Number,
 max_credits: Number,
 min_discount: Number,
 discount_profit_percentage: Number,
-min_monthly_rate: Number,
+monthly_lending_rate: Number,
 loan_risk: Number,
 members_served_percentage: Number,
 loan_multiple: Number
@@ -18,3 +18,4 @@ loan_multiple: Number
 
 const Constants = mongoose.model('constant', constantsSchema);
 module.exports = Constants;
+
