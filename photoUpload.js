0989e16b8584bd17f9a71b29c579d7afed0fc006
Ariko <philemonarik@gmail.com>
2024-09-15 18:05:29 +0300

    fixed photo bug

--- photoUpload.js
+++ photoUpload.js
@@ -10,25 +10,28 @@ const upload = multer({ dest: 'uploads/'  
 
 Router.post('/upload-photo', upload.single('image'), async (req, res) => {
   const userId = req.user._id;
-  const fileName = `${userId}.jpg`;
+  const fileName = `${userId}-${new Date().toDateString()}.jpg`;
   const filePath = path.join('public', 'img', fileName);
 
   // Replace existing file if necessary
-  fs.rename(req.file.path, filePath, (err) => {
+  fs.rename(req.file.path, filePath, async (err) => {
     if (err) {
+      console.log(err)
       return res.status(500).json({ msg: "Upload Failed" });
     }
+    const user = req.user;
+    user.photoURL = "img/" + req.user._id +  ".jpg"
+    user.photoURL = `img/${req.user._id}-${new Date().toDateString()}.jpg`
+    await user.save()
+    return res.json({msg: "Upload Successful"})
   });
-  const user = req.user;
-  user.photoURL = "img/" + req.user._id + ".jpg"
-  await user.save()
-  return res.json({msg: "Upload Successful"})
+
 });
 
 Router.delete('/delete-photo', async (req, res) => {
-  const userId = req.user._id;
-  const fileName = `${userId}.jpg`;
-  const filePath = path.join('public', 'img', fileName);
+
+  const fileName = req.user.photoURL
+  const filePath = path.join('public', fileName);
 
   fs.unlink(filePath, (err) => {
     if (err && err.code !== 'ENOENT') { // Ignore if file doesn't exist
