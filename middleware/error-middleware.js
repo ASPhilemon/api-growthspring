49c9bb095095a97ddd7a74350df5d51b5902ec52
ASPhilemon <philemonariko@gmail.com>
2025-05-21 01:41:25 +0300

    updated error middleware to use http-response-util for sending error response

--- middleware/error-middleware.js
+++ middleware/error-middleware.js
@@ -1,10 +1,10 @@
-// middleware/errorHandler.js
 import { AppError } from '../utils/error-util.js';
+import * as Response from "../utils/http-response-util.js"
 
 export function errorHandler(err, req, res, next) {
   console.log(err)
   const isAppError = err instanceof AppError;
   const statusCode = isAppError ? err.statusCode : 500;
   const message = isAppError ? err.message : 'Sorry, an unknown error occured';
-  res.status(statusCode).json({error: message, data: null})
+  Response.sendError(res, message, statusCode)
 }
