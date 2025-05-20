b338a46fd57dfc5c4e23c0ca0990d8325ddc2403
ASPhilemon <philemonariko@gmail.com>
2025-05-21 01:47:36 +0300

    feature: implemented deposit service

--- services/deposit-service/controller.js
+++ services/deposit-service/controller.js
@@ -1,21 +1,33 @@
 import * as ServiceManager from "./service.js"
+import * as Response from "../../utils/http-response-util.js"
 
 export async function getDeposits(req, res){
-
+  const {filter, sort, pagination} = req.query
+  const deposits = await ServiceManager.getDeposits({ filter, sort, pagination})
+  Response.sendSuccess(res, deposits)
 }
 
 export async function getDeposit(req, res){
-
+  const { id: depositId }= req.params
+  const deposit = await ServiceManager.getDeposit(depositId)
+  Response.sendSuccess(res, deposit)
 }
 
 export async function createDeposit(req, res){
-
+  const { deposit } = req.body
+  await ServiceManager.createDeposit(deposit)
+  Response.sendSuccess(res, null)
 }
 
 export async function updateDeposit(req, res){
-
+  const { id: depositId } = req.params
+  const { update } = req.body
+  await ServiceManager.updateDeposit(depositId, update)
+  Response.sendSuccess(res, null)
 }
 
 export async function deleteDeposit(req, res){
-  
+  const { id: depositId }= req.params
+  await ServiceManager.deleteDeposit(depositId)
+  Response.sendSuccess(res, null)
 }
--- services/deposit-service/service.js
+++ services/deposit-service/service.js
@@ -1,20 +1,78 @@
+//model
 import { Deposit } from "./models.js"
 
-export async function getDeposits(){
+//util
+import * as DB from "../../utils/db-util.js"
+
+//collaborator services
+import * as UserServiceManager from "../user-service/service.js"
+import * as EmailServiceManager from "../email-service/service.js"
+import * as CashLocationServiceManager from "../cash-location-service/service.js"
+
+export async function getDeposits({ filter, sort, pagination }){
+  return await Deposit.getDeposits({filter, sort, pagination})
 }
 
-export async function getDeposit(){
-
+export async function getDeposit(depositId){
+  const deposit = await DB.tryMongoose(Deposit.findById(depositId))
+  const statusCode = 400
+  if (!deposit) throw new ErrorUtil.AppError("Failed to find deposit", statusCode)
+  return deposit
 }
 
-export async function createDeposit(){
+export async function createDeposit(deposit){
+  //get user
+  const { userId } = deposit.depositor
+  const user = await UserServiceManager.getUser(userId)
 
+  //add field balance before to deposit
+  deposit.balance_before = user.investmentAmount
+
+  const userUpdate = { investmentAmount : user.investmentAmount + deposit.amount }
+  
+  await Promise.all([
+    DB.tryMongoose(Deposit.create(deposit)),
+    UserServiceManager.updateUser(user._id, userUpdate),
+    CashLocationServiceManager.addToCashLocation(deposit.cashLocationId, deposit.amount)
+  ])
+  
+  EmailServiceManager.sendEmail({
+    sender: "growthspring",
+    recipient: user.email,
+    subject: "Deposit Recorded",
+    message: "Your deposit has been recorded."
+  })
 }
 
-export async function updateDeposit(){
+export async function updateDeposit(depositId, update){
+  const deposit = await getDeposit(depositId)
 
+  const { userId } = deposit.depositor
+  const user = await UserServiceManager.getUser(userId)
+
+  //update user and deposit
+  const investmentAmountUpdate = user.investmentAmount - deposit.amount + update.amount
+  const userUpdate = {investmentAmount: investmentAmountUpdate  }
+
+  await Promise.all([
+    UserServiceManager.updateUser(userId, userUpdate ),
+    DB.tryMongoose(Deposit.updateOne({ _id: deposit._id }, update))
+  ])
 }
 
-export async function deleteDeposit(){
+export async function deleteDeposit(depositId) {
+  const deposit = await getDeposit(depositId)
 
+  const { userId } = deposit.depositor
+  const user = await UserServiceManager.getUser(userId)
+    
+  const updatedInvestmentAmount = user.investmentAmount - deposit.amount
+  const userUpdate = { investmentAmount: updatedInvestmentAmount }
+  const { cashLocationId } = deposit.cashLocation
+
+  await Promise.all([
+    UserServiceManager.updateUser(userId, userUpdate),
+    CashLocationServiceManager.deductFromCashLocation(cashLocationId, deposit.amount),
+    DB.tryMongoose(Deposit.deleteOne({ _id: depositId }))
+  ])
 }
