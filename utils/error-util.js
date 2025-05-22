7758dbc88cdbbedb405a273365cbfc9c4c7627fc
ASPhilemon <philemonariko@gmail.com>
2025-05-22 07:05:18 +0300

    refactor: extended AppError class with more specific error types

--- utils/error-util.js
+++ utils/error-util.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose"
 
 export class AppError extends Error {
-  constructor(message, statusCode, cause) {
+  constructor({ message, statusCode, cause = null}) {
     super(message);
     this.statusCode = statusCode;
     this.cause = cause
@@ -9,16 +9,48 @@ export class AppError extends Error {
   }
 }
 
+export class NotAuthenticatedError extends AppError {
+  constructor({message = "Failed to authenticate request", cause = null}={}) {
+    const statusCode = 401
+    super({message, statusCode, cause});
+  }
+}
+
+export class NotAllowedError extends AppError {
+  constructor({message = "Access Denied", cause = null}={}) {
+    const statusCode = 403
+    super({message, statusCode, cause});
+  }
+}
+
+export class BadRequestError extends AppError {
+  constructor({message = "There was something wrong in your request", cause = null}={}) {
+    const statusCode = 400
+    super({message, statusCode, cause});
+  }
+}
+
+export class NotFoundError extends AppError {
+  constructor({message = "Resource not found", cause = null}={}) {
+    const statusCode = 404
+    super({message, statusCode, cause});
+  }
+}
+
+export class InternalServerError extends AppError {
+  constructor({message = "Sorry, something went wrong on the server", cause = null}={}) {
+    const statusCode = 500
+    super({message, statusCode, cause});
+  }
+}
+
 export function handleMongooseError(err){
   const { ValidationError, CastError } = mongoose.Error
 
-  let statusCode
   if (err instanceof ValidationError || err instanceof CastError){
-    statusCode = 400
-    throw new AppError("Failed to validate user input", statusCode, err)
+    throw new BadRequestError({message: "Failed to validate user input", cause: err})
   }
   else{
-    statusCode =  500
-    throw new AppError("A database error occured", statusCode, err)
+    throw new InternalServerError()
   }
 }
