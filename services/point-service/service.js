f975eb04a54a1e9f5c632151377bdd01e0cef4f7
ASPhilemon <philemonariko@gmail.com>
2025-06-16 22:24:36 +0300

    refactor: refactored point service recordTransaction: redeem, reward, transfer

--- services/point-service/service.js
+++ services/point-service/service.js
@@ -1,6 +1,7 @@
 import { PointTransaction } from "./models.js"
 import * as DB from "../../utils/db-util.js"
-import * as ErrorUtil from "../../utils/error-util.js"
+import * as Errors from "../../utils/error-util.js"
+import * as Validator from "../../utils/validator.js"
 
 //collaborator services
 import * as UserServiceManager from "../user-service/service.js"
@@ -12,28 +13,68 @@ export async function getTransactions(filter){
 
 export async function getTransactionById(transactionId){
   const transaction =  DB.query(PointTransaction.findById(transactionId))
-  if (!transaction) throw new ErrorUtil.NotFoundError("Transaction not found");
+  Validator.assert(transaction, "Failed to find trasaction", {errType:Errors.NotFoundError})
   return transaction
 }
 
-export async function recordTransaction(transaction){
-  const {type, recipientId, senderId, reason, redeemedById, points:amountPoints} = transaction
-  if (recipientId) await UserServiceManager.updateUser(recipientId, {$inc:{points: amountPoints }});
-  if (senderId) await UserServiceManager.updateUser(senderId, {$inc:{points: -amountPoints }});
-  if (redeemedById) await UserServiceManager.updateUser(redeemedById, {$inc:{points: -amountPoints }});
-  await DB.query(PointTransaction.create(transaction))
+export async function getTransactionByRefId(refId){
+  const transaction =  DB.query(PointTransaction.findOne({refId}))
+  Validator.assert(transaction, "Failed to find transaction", {errType:Errors.NotFoundError})
+  return transaction
 }
 
-export async function setPoints(filter, newPoints){
-  const result = await DB.query(PointTransaction.updateOne({filter}, {$set: {points: newPoints}}))
-  if (result.matchedCount == 0){
-    throw new ErrorUtil.BadRequestError("Failed to find point transaction")
-  }
+export async function rewardUser(userId, points, reason, refId){
+  const user = await UserServiceManager.getUserById(userId)
+  await DB.transaction(async()=>{
+    await UserServiceManager.addPoints(userId, points)
+    await PointTransaction.create({
+      type: "reward",
+      recipient: {_id: user._id, fullName: user.fullName},
+      points,
+      reason,
+      refId
+    })
+  })
 }
 
-export async function deleteTransaction(filter){
-  const result = await DB.query(PointTransaction.findOneAndDelete(filter))
-  if (result.matchedCount == 0){
-    throw new ErrorUtil.BadRequestError("Failed to find transaction to delete")
-  }
+export async function redeemPoints(userId, points, reason, refId){
+  const user = await UserServiceManager.getUserById(userId)
+  await DB.transaction(async()=>{
+    await UserServiceManager.deductPoints(userId, points)
+    await PointTransaction.create({
+      type: "redeem",
+      redeemedBy: {_id: user._id, fullName: user.fullName},
+      points,
+      reason,
+      refId
+    })
+  })
+}
+
+export async function transferPoints(senderId, recipientId, points, reason){
+  const [sender, recipient] = await Promise.all([
+    UserServiceManager.getUserById(senderId),
+    UserServiceManager.getUserById(recipientId)
+  ])
+  await DB.transaction(async()=>{
+    await UserServiceManager.deductPoints(senderId, points)
+    await UserServiceManager.addPoints(recipientId, points)
+    await PointTransaction.create({
+      type: "transfer",
+      sender: {_id: user._id, fullName: user.fullName},
+      recipient: {_id: user._id, fullName: user.fullName},
+      points,
+      reason,
+    })
+  })
+}
+
+export async function deleteTransactionById(transactionId){
+  const result = await DB.query(PointTransaction.findOneAndDelete({_id: transactionId}))
+  Validator.assert(result.matchedCount !== 0, "Failed to find point transaction to delete")
+}
+
+export async function deleteTransactionByRefId(refId){
+  const result = await DB.query(PointTransaction.findOneAndDelete({refId}))
+  Validator.assert(result.matchedCount !== 0, "Failed to find point transaction to delete")
 }
