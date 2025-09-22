89a17e015a98a8221410114cfa7fd0d7d24d8a0b
ASPhilemon <philemonariko@gmail.com>
2025-09-22 22:48:17 +0300

    update main with v2

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import dotenv from 'dotenv';
 import connectDB from "./db.js"
 import app from "./app.js"
 
-//load environemnt variables
+//load environemnt variables 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const ENV_FILE = process.env.NODE_ENV === "production"?
