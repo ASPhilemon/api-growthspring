7d18a2c7db4c3a71a2005bae53145eeb54f2c860
Ariko <philemonariko@gmail.com>
2026-07-08 08:05:44 +0300

    hotfix

--- server.js
+++ server.js
@@ -8,20 +8,6 @@ import app from "./app.js"
 import { AppError } from './utils/error-util.js';
 import { sendEmail } from './services/email-service/service.js';
 
-
-//load environemnt variables 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const ENV_FILE = process.env.NODE_ENV === "production"?
-".env.production" :
-".env"
-const ENV_FILE_PATH = path.join(__dirname, ENV_FILE)
-if (!fs.existsSync(ENV_FILE_PATH)) {
-  throw new Error(`Environment file not found: ${ENV_FILE_PATH}`);
-}
-dotenv.config({ path: ENV_FILE_PATH, override: true})
-
-
 //connect to database
 const MONGODB_URI = process.env.MONGODB_URI
 await connectDB(MONGODB_URI)
