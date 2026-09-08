71512e85a5c896510da702cd0b9c79d83a47fbef
Philemon <philemonariko@gmail.com>
2026-09-09 02:18:12 +0300

    Birthdays

--- services/user-service/cron.js
+++ services/user-service/cron.js
@@ -2,13 +2,13 @@ import nodeCron from "node-cron";
 import * as UserServiceManager from "./service.js"
 
 export function schedule(){
-    // Runs every day at 7:00 HRS(7AM) AM UTC => 10AM EAT
+    // Runs every day at 7:00 HRS(7AM) UTC => 10AM EAT
     nodeCron.schedule('0 7 * * *', ()=>{
         UserServiceManager.sendBirthdayReminder()
         console.log("Birthday Reminder Executed ...")
     },{timezone: 'UTC'})
 
-    // Runs every day at 7:00 HRS(7AM) AM UTC => 10AM EAT
+    // Runs every day at 7:00 HRS(7AM) UTC => 10AM EAT
     nodeCron.schedule('0 7 * * *', ()=>{
         UserServiceManager.sendBirthdayCelebration()
         console.log("Birthday Celebration Executed ...")
